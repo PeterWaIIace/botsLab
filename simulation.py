2398080d2acf7a5ce803ad00dd5f00e09d889542
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-04-17 11:54:05 +0100

    update of simulation

--- simulation.py
+++ simulation.py
@@ -1,4 +1,11 @@
 import pybullet as p
+import time 
 
 physicsClient = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(0.01)
+
 p.disconnect()
