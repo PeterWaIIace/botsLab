720f0ad59bbdd5c480453bf47ac8b398a54c1a33
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-04-18 23:32:47 +0100

    added sensors

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time 
@@ -13,13 +14,17 @@ p.setGravity(0,0,-9.8*10)
 # load world plane
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
-
+pyrosim.Prepare_To_Simulate(robotId)
 #load box object
 p.loadSDF("world.sdf")
 
 #simulate
 for _ in range(100000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    print(backLegTouch,frontLegTouch)
+
     time.sleep(0.01)
 
 p.disconnect()
